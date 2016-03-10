9d364b85a2cc35f5ec8131274b0be21f7c9d4f00
Itamar Kaufman <itamarkaufman@itamars-MacBook-Pro-2.local>
2016-03-10 22:46:45 +0200

    fab and more

--- app/src/main/java/org/androidtlv/autonomy/MainActivity.java
+++ app/src/main/java/org/androidtlv/autonomy/MainActivity.java
@@ -91,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
                         .setContentText(stringAlert);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(notificationId, notificationBuilder.build());
-
     }
 }
 
